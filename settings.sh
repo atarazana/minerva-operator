c1c5f159e4bac3adfd371e37d0b79404c2aa6f96
cvicens <carlos.vicens.alonso@gmail.com>
2020-11-16 13:01:23 +0100

    init

---
+++ settings.sh
@@ -0,0 +1,15 @@
+export GO111MODULE=on
+
+export APP_NAME=minerva
+export OPERATOR_NAME=${APP_NAME}-operator
+export OPERATOR_IMAGE=${OPERATOR_NAME}-image
+
+export ORGANIZATION=atarazana
+export DOMAIN=${ORGANIZATION}.com
+export API_VERSION=${APP_NAME}.${DOMAIN}/v1
+
+export PROJECT_NAME=${OPERATOR_NAME}-system
+
+export USERNAME=cvicens
+
+export VERSION=0.0.1
